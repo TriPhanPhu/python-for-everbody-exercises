71b8201967f3c332edf82b681449236bea39aaf1
Tri Phan <phutridanang@gmail.com>
2019-09-04 16:06:15 +0700

    Add a demo file

---
+++ chapter09_dictionaries_example_histogram.py
@@ -0,0 +1,15 @@
+word = 'brontosaurus'
+
+d = dict()
+for c in word:
+    if not c in d: 
+        d[c] = 1
+    else: 
+        d[c] = d[c] + 1
+print(d)
+
+# Using the get method
+d2 = dict()
+for c in word:
+    d2[c] = d2.get(c, 0) + 1
+print(d2)
